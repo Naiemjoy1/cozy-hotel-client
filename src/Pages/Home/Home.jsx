a2d45d0aa8de74dd11de37d6dfed46fa5cbc83f0
Naiemjoy1 <naiemjoy1@gmail.com>
2024-05-12 23:55:17 +0600

    home modal added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,10 +3,14 @@ import HotelRooms from "../../Components/HotelRooms/HotelRooms";
 import Map from "../../Components/Map/Map";
 import Review from "../../Components/Review/Review";
 import Banner from "./Banner";
+import HomePageModal from "./HomePageModal";
+import ServerModal from "./ServerModal";
 
 const Home = () => {
   return (
     <div>
+      <HomePageModal></HomePageModal>
+      {/* <ServerModal></ServerModal> */}
       <Banner></Banner>
       <HotelRooms></HotelRooms>
       <Map></Map>
