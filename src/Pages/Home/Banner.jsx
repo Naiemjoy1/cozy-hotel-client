7fa9bb7baaaf3b3575c8eccb69687505f68fb89b
Naiemjoy1 <naiemjoy1@gmail.com>
2024-05-14 17:05:21 +0600

    small update

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -1,9 +1,29 @@
-import React from "react";
-
 const Banner = () => {
   return (
     <div>
-      <h2>hi</h2>
+      <div
+        className="hero"
+        style={{
+          backgroundImage:
+            "url(https://i.ibb.co/qBgDtVR/pexels-pixabay-261388.jpg)",
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className=" flex justify-center items-center text-center text-white lg:container lg:mx-auto gap-10 py-28">
+          <div className="lg:w-1/2 space-y-5">
+            <p className=" font-roboto font-semibold">BEST PLACE FOR RELAX</p>
+            <p className=" font-marcellus font-normal text-2xl lg:text-5xl">
+              Perfect Countryside Vacation at CozyStay Lodge
+            </p>
+            <p className=" font-roboto">
+              Nestled in Napa Valley, CozyStay Lodge is a luxury boutique hotel
+              in the heart of wine country, conveniently located in the historic
+              Napa Mill neighbourhood, just steps from some of the best wineries
+              and restaurants.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
